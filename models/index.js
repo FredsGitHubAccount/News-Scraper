e517c3af29f4933fe8ffe85617da09fe8edb1801
Fred Saba <fred@fred-saba.com>
2019-10-26 23:48:13 -0700

    fixing heroku

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Article: require("./Article.js"),
-    Note: require("./Note.js")
+    Article: require("./article.js"),
+    Note: require("./note.js")
   };
   
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,6 @@
 let axios = require('axios')
 let cheerio = require('cheerio')
-let db = require('../models');
+let db = require("../models");
 let mongoose = require("mongoose")
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articleScrapper";
